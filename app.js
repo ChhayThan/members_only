54d33c63d15c0dba8b8f5c82569fc35a22f261a6
EricChhour <75393200+ChhayThan@users.noreply.github.com>
2024-08-30 12:31:08 +0700

    Initial Project commit.
    Setup project structure - folders and file management.

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const indexRouter = require("./routes/indexRouter");
+
+app.use(express.urlencoded({ extended: true }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running: Port ${PORT}`));
